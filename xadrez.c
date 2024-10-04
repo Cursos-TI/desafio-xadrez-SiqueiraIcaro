5cbdf42a631908fa1dcc03ea67a8b92fd1d1d084
SiqueiraIcaro <siqueiraicaro2002@gmail.com>
2024-10-04 23:28:14 +0000

    Criei a movimentação das peças

--- xadrez.c
+++ xadrez.c
@@ -7,26 +7,37 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    
+    const int mov_bispo = 5;
+    const int mov_torre = 5;
+    const int mov_rainha = 8;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
+    printf("movimento do bispo (diagonal superior direita) \n");
+    for (int i = 1; i <= mov_bispo; i++){
+        printf("Bispo se moveu %d casas na diagonal superior direita \n", i);
+    }
+
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    printf("movimento da torre \n");
+    for (int i = 1; i <= mov_torre; i++){
+        printf("Torre se moveu %d casas para direita \n", i);
+    }
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    
+    printf("movimento da rainha para esquerda \n");
+    
+    
+    for (int i = 1; i <= mov_rainha; i++){
+        printf("Rainha se moveu %d casas para esquerda \n", i);
+    }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
     return 0;
 }
